2f01db7624bd8a382877bc12f4717571f3199abb
Luis Enrique <fernandomorenoluis123@gmail.com>
2025-07-18 21:53:19 -0600

    Final, early version

--- Projects/prject1_codedex.py
+++ Projects/prject1_codedex.py
@@ -11,19 +11,34 @@ def main ():
     '''
     
     
-    print('WELCOME TO THE GAME OF ROCK, PAPER AND SCISSORS')
+    print('''
+        ================================
+        Rock Paper Scissors Lizard Spock
+        ================================
+            ''')
     
-    options = [1,5]
+    print('''
+        1) ✊
+        2) ✋
+        3) ✌️
+        4) 🦎
+        5) 🖖
+                ''')
     
-    player = int(input("Pick a number: 1 '✊' (Rock), 2 is for '✋' (Paper), 3 is for '✌️' (Scissors), 4 is for '🦎' and 5 is for '🖖'"))
+    
+    
+    options = [1,2,3,4,5]
+    
+    
+    player = int(input("Pick a number: "))
     
     if player not in options:
         return 'Incorrect option'
     
     computer = random.randint(1,5)
     
-    print(f"Your choise was {'✊' if player == 1 else '✋'if  player == 2 else '✌️' if player == 3 else '🦎' if player == 4 else }")
-    print(f"The computer choise was {'✊' if computer == 1 else '✋'if  computer == 2 else '✌️'}")
+    print(f"Your choise was {'✊' if player == 1 else '✋'if  player == 2 else '✌️' if player == 3 else '🦎' if player == 4 else '🖖' }")
+    print(f"The computer choise was {'✊' if computer == 1 else '✋'if  computer == 2 else '✌️' if computer == 3 else '🦎' if computer == 4 else '🖖' }")
     print(ganador(player, computer))
     
     
@@ -33,10 +48,11 @@ def ganador (player,computer):
     if player == computer:
         return 'It is a draw'
     elif (
-            (player == 1 and computer == 3) or
-            (player == 2 and computer == 1) or
-            (player == 3 and computer == 2) or
-            (player == 4 and computer ==) 
+            (player == 1 and (computer == 3 or computer == 4)) or
+            (player == 2 and (computer == 1 or computer == 5)) or
+            (player == 3 and (computer == 2 or computer == 4)) or
+            (player == 4 and (computer == 2 or computer == 5)) or
+            (player == 5 and (computer == 3 or computer == 1)) 
         ):
         return 'You Won'
     else:
